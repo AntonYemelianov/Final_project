171ac11c097b2ca387b03f3109ea74d2ca3b0840
AntonYemelianov <ey355@drexel.edu>
2026-05-26 17:09:55 -0400

    Update architectural_geometry.py

--- architectural_geometry.py
+++ architectural_geometry.py
@@ -1,1 +1,28 @@
 import maya.cmds as cmds
+
+DEBUG = True
+
+
+def create_floor(floor_width=(random.uniform(0.1, 50)),
+                 floor_thickness=(random.uniform(0.1, 50)), 
+                 floor_length=(random.uniform(0.1, 50)),
+                 position=(0, 0 ,0):
+    """Create a floor
+
+    Args:
+        length (float):    Floor length. Random value between 0.1 and 50.
+        thickness (float): Floor thickness. Random value between 0.1 and 50.
+        width (float):     Floor width. Random value between 0.1 and 50.
+        position (tuple):  (x, y, z) center of the floor base.
+
+    Returns:
+        Str: Name of the floor transform node or none if failure.
+    """
+
+    try:
+        floor = cmds.polyCube(width=floor_width, 
+                          height=floor_thickness,
+                          depth=floor_length,
+                          name="floor_#"
+        )[0]
+        cmds.move
