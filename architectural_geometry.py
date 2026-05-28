ca7b3f9461ca1bdba93e2f5fc112e5cb835c2354
AntonYemelianov <ey355@drexel.edu>
2026-05-28 10:18:35 -0400

    Update architectural_geometry.py

--- architectural_geometry.py
+++ architectural_geometry.py
@@ -3,21 +3,32 @@ import maya.cmds as cmds
 DEBUG = True
 
 
-def create_floor(floor_width=(random.uniform(0.1, 50)),
-                 floor_thickness=(random.uniform(0.1, 50)), 
-                 floor_length=(random.uniform(0.1, 50)),
+def create_floor(floor_width=5, floor_thickness=0.5, floor_length=5,
                  position=(0, 0 ,0):
     """Create a floor
 
     Args:
-        length (float):    Floor length. Random value between 0.1 and 50.
-        thickness (float): Floor thickness. Random value between 0.1 and 50.
-        width (float):     Floor width. Random value between 0.1 and 50.
+        floor_width (float):    Floor width. Default 5.
+        floor_thickness (float): Floor thickness. Default 0.5.
+        floor_length (float):   Floor length. Default 5.
         position (tuple):  (x, y, z) center of the floor base.
 
     Returns:
         Str: Name of the floor transform node or none if failure.
     """
+if DEBUG:
+    print("[DEBUG] create_floor: fl={}, fh{}, fd={}, pos={}".format(
+      floor_width, floor_thickness, floor_length, position))
+
+if floor_width < 1:
+    cmds.warning("invalid floor width {} -- using default 5".format(floor_width))
+    width=10
+if floor_thickness < 0.1:
+    cmds.warning("invalid floor thickness {} -- using default 0.5".format(floor_thickness))
+    width=0.5
+if floor_length < 1:
+    cmds.warning("invalid floor length {} -- using default 5".format(floor_length))
+    width=10
 
     try:
         floor = cmds.polyCube(width=floor_width, 
@@ -28,3 +39,8 @@ def create_floor(floor_width=(random.uniform(0.1, 50)),
         cmds.move(
             (position[0], position[1] / 2.0, position[2], wall
         )
+    except Exception as error:
+      cmds.warning("Failed to create floor: {}".format(error))
+      return none
+
+return floor
