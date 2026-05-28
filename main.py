0b74c888a8bdd782a5bfee7b2a478c199de7f5dd
AntonYemelianov <ey355@drexel.edu>
2026-05-28 10:56:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+"""
+main.py -- House Generator (DIGM Final)
+=======================================
+Author: Anton Yemelianov
 
+Assembles a house from configuration data using the builders dispatcher pattern.
+"""
+
+import maya.cmds as cmds
+import architectural_geomentry as geo
+
+
+#Confirguration Data
+
+#Dispatcher
+
+#Driver
+
+#Main
