4bb99e034e44a08ba497c2ab9d36deba703790b3
AntonYemelianov <ey355@drexel.edu>
2026-06-04 10:13:39 -0400

    Update architectural_geometry.py

--- architectural_geometry.py
+++ architectural_geometry.py
@@ -3,32 +3,32 @@ import maya.cmds as cmds
 DEBUG = True
 
 
-def create_floor(floor_width=5.0, floor_thickness=0.5, floor_length=5.0,
+def create_body(body_width=5.0, body_height=0.5, body_length=5.0,
                  position=(0, 0 ,0):
-    """Create a rectangular floor
+    """Create a rectangular body of the building
 
     Args:
-        floor_width (float):     Floor width. Default 5.
-        floor_thickness (float): Floor thickness. Default 0.5.
-        floor_length (float):    Floor length. Default 5.
-        position (tuple):  (x, y, z) center of the floor base.
+        body_width (float):     Body width. Default 5.
+        body_thickness (float): Body Height. Default 3.
+        body_length (float):    Body length. Default 5.
+        position (tuple):  (x, y, z) center of the body.
 
     Returns:
         Str: Name of the floor transform node or none if failure.
     """
 if DEBUG:
-    print("[DEBUG] create_floor: fw={}, ft{}, fl={}, pos={}".format(
-      floor_width, floor_thickness, floor_length, position))
+    print("[DEBUG] create_body: bw={}, bh{}, bl={}, pos={}".format(
+      body_width, body_height, body_length, position))
 
-if floor_width < 1.0:
-    cmds.warning("invalid floor width {} -- using default 5".format(floor_width))
-    floor_width=5
-if floor_thickness < 0.1:
-    cmds.warning("invalid floor thickness {} -- using default 0.5".format(floor_thickness))
-    floor_thickness=0.
-if floor_length < 1.0:
-    cmds.warning("invalid floor length {} -- using default 5".format(floor_length))
-    floor_length=5
+if body_width < 1.0:
+    cmds.warning("invalid body width {} -- using default 5".format(body_width))
+    body_width=5
+if body_height < 0.5:
+    cmds.warning("invalid body height {} -- using default 0.5".format(body_height))
+    body_height=0.
+if body_length < 1.0:
+    cmds.warning("invalid body length {} -- using default 5".format(body_length))
+    body=length=5
 
     try:
         floor = cmds.polyCube(width=floor_width, 
@@ -40,49 +40,58 @@ if floor_length < 1.0:
             (position[0], position[1] + floor_thickness / 2.0, position[2], wall
         )
     except Exception as error:
-      cmds.warning("Failed to create floor: {}".format(error))
+      cmds.warning("Failed to create body: {}".format(error))
       return none
 
-return floor
+return body
 
 
-def create_wall(wall_length=5.0, wall_height=3.0, wall_thickness=0.2,
+def create_porch(porch_width=1, porch_height=0.5, porch_width=1,
+                 stair_width=porch_width / 2.0,
+                 stair_height=porch height / 4.0,
                  position=(0, 0 ,0):
-    """Create a  rectangular wall
+    """Create a rectangular porch with some stairs 
 
     Args:
-        wall_length (float):    Floor width. Default 5.
-        wall_height (float):    Floor thickness. Default 3.
-        wall_thickness (float): Floor length. Default 0.2.
+        porch_length (float):    Porch length. Default 5.
+        porch_height (float):    Porch height. Default 3.
+        porch_width (float): Floor length. Default 0.2.
         position (tuple):  (x, y, z) center of the floor base.
 
     Returns:
         Str: Name of the wall transform node or none if failure.
     """
 if DEBUG:
-    print("[DEBUG] create_wall: wl={}, wh{}, wt={}, pos={}".format(
-      wall_length, wall_height, wall_thickness, position))
+    print("[DEBUG] create_porrch: pw={}, ph{}, pl={}, pos={}".format(
+      porch_width, porch_height, porch_thickness, position))
 
-if wall_length < 1:
-    cmds.warning("invalid wall length {} -- using default 5".format(wall_length))
-    wall_length=10
-if wall_height < 0.1:
-    cmds.warning("invalid wall height {} -- using default 0.5".format(wall_height))
-    wall_height=0.5
-if wall_thickness < 1:
-    cmds.warning("invalid wall thickness {} -- using default 5".format(wall_thickness))
-    wall_thickness=10
+if porch_width < 0.1:
+    cmds.warning("invalid porch width {} -- using default 5".format(porch_length))
+    wall_width = 1
+if porch_height < 0.05:
+    cmds.warning("invalid porch height {} -- using default 0.5".format(porch_height))
+    wall_height = 0.5
+if porch_length < 0.1:
+    cmds.warning("invalid porch thickness {} -- using default 5".format(porch_length))
+    porch_length = 10
 
     try:
-        wall = cmds.polyCube(width=wall_length, 
-                          height=wall_height,
-                          depth=wall_thickness,
-                          name="wall_#"
+        porch = cmds.polyCube(width=porch_width, 
+                          height=porch_height,
+                          depth=porch_length,
+                          name="porch_#"
         )[0]
         cmds.move(
-            (position[0], position[1] + wall_height/ 2.0, position[2], wall
+            (position[0], position[1] + porch_height/ 2.0, position[2], porch
         )
+        stair1 = cmds.polyCube(width=stair_width, 
+                          height=stair_height,
+                          depth=porch_length,
+                          name="stair_#
+        
     except Exception as error:
+    cmds.warning("Failed to create porch: {}".format(error))
+      return none
 
 def create_roof(roof_width=floor_width+wall_thickness+0.1,
                roof_height=0.5,
@@ -117,5 +126,14 @@ try:
             depth=roof_length,
             name="roof_#"
         )[0]
+  cmds.move(
+      (position[0], position[1] / 2.0, position[2], roof
+  )
+    except Exception as error:
+      cmds.warning("Failed to create roof: {}".format(error))
+      return None
+
+    return wall
+
       
                  
