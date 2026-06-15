486698e0888aab997e2e6dd00191454795f43319
AntonYemelianov <ey355@drexel.edu>
2026-06-15 02:23:06 -0400

    Update architectural_geometry.py

--- architectural_geometry.py
+++ architectural_geometry.py
@@ -3,8 +3,8 @@ import maya.cmds as cmds
 DEBUG = True
 
 
-def create_body(body_width=5.0, body_height=0.5, body_length=5.0,
-                 position=(0, 0 ,0):
+def create_body(body_width=5.0, body_height=0.3, body_length=5.0,
+                position=(0, 0 ,0)):
     """Create a rectangular body of the building
 
     Args:
@@ -14,42 +14,89 @@ def create_body(body_width=5.0, body_height=0.5, body_length=5.0,
         position (tuple):  (x, y, z) center of the body.
 
     Returns:
-        Str: Name of the floor transform node or none if failure.
+        Str: Name of the body transform node or none if failure.
     """
-if DEBUG:
-    print("[DEBUG] create_body: bw={}, bh{}, bl={}, pos={}".format(
-      body_width, body_height, body_length, position))
+    if DEBUG:
+        print("[DEBUG] create_body: bw={}, bh{}, bl={}, pos={}".format(
+        body_width, body_height, body_length, position))
 
-if body_width < 1.0:
-    cmds.warning("invalid body width {} -- using default 5".format(body_width))
-    body_width=5
-if body_height < 0.5:
-    cmds.warning("invalid body height {} -- using default 0.5".format(body_height))
-    body_height=0.
-if body_length < 1.0:
-    cmds.warning("invalid body length {} -- using default 5".format(body_length))
-    body=length=5
+    if body_width < 1.0:
+        cmds.warning("invalid body width {} -- using default 5".format(body_width))
+        body_width=5
+    if body_height < 0.5:
+        cmds.warning("invalid body height {} -- using default 0.5".format(body_height))
+        body_height=3
+    if body_length < 1.0:
+        cmds.warning("invalid body length {} -- using default 5".format(body_length))
+        body=length=5
 
     try:
-        floor = cmds.polyCube(width=floor_width, 
-                          height=floor_thickness,
-                          depth=floor_length,
-                          name="floor_#"
+        floor = cmds.polyCube(width=body_width, 
+                          height=body_height,
+                          depth=body_length,
+                          name="body_#"
         )[0]
         cmds.move(
             (position[0], position[1] + floor_thickness / 2.0, position[2], wall
         )
+      except Exception as error:
+          cmds.warning("Failed to create body: {}".format(error))
+          return none
+
+      return body
+
+def create_door(door_width=0.5, door_height=1, door_thickness=0.1,
+                position=(0, 0, 0)):
+    """Create a rectangular body of the building
+
+    Args:
+        door_width (float):     Body width. Default 0.5.
+        door_height (float):    Body Height. Default 1.
+        door_thickness (float): Body length. Default 0.1.
+        position (tuple):  (x, y, z) center of the body.
+
+    Returns:
+        Str: Name of the door transform node or none if failure.
+    """
+    if DEBUG:
+        print("[DEBUG] create_door: dw={}, dh{}, dt={}, pos={}".format(
+      door_width, door_height, door_thickness, position))
+
+    if door_width < 0.1:
+        cmds.warning("invalid door width {} -- using default 0.5".format(door_width))
+        door_width = 0.5
+    if door_height < 0.2:
+        cmds.warning("invalid door height {} -- using default 1".format(door_height))
+        door_height = 1
+    if door_thickness < 0.05:
+        cmds.warning("invalid door thickness {} -- using default 0.1".format(door_length))
+        door_thickness = 0.1
+      
+    try:
+        door = cmds.polyCube(
+                             width=door_width, 
+                             height=door_height,
+                             depth=door_thickness,
+                             name="door_#"
+        )[0]
+        cmds.move(
+            position[0],
+            position[1] + door_height / 2.0 + porch_height,
+            position[2] + body_length / 2.0,
+            door
+        )
     except Exception as error:
-      cmds.warning("Failed to create body: {}".format(error))
-      return none
-
-return body
-
+        cmds.warning(warning("Failed to create door: {}".format(error))
+        return None
+      
+    return door
+        
+      
 
 def create_porch(porch_width=1, porch_height=0.5, porch_width=1,
                  stair_width=porch_width / 2.0,
                  stair_height=porch height / 4.0,
-                 position=(0, 0 ,0):
+                 position=(0, 0 ,0)):
     """Create a rectangular porch with some stairs 
 
     Args:
@@ -75,12 +122,12 @@ if porch_length < 0.1:
     cmds.warning("invalid porch thickness {} -- using default 5".format(porch_length))
     porch_length = 10
 
-    try:
-        porch = cmds.polyCube(width=porch_width, 
+try:
+    porch = cmds.polyCube(width=porch_width, 
                           height=porch_height,
                           depth=porch_length,
                           name="porch_#"
-        )[0]
+    )[0]
         cmds.move(
             (position[0], position[1] + porch_height/ 2.0, position[2], porch
         )
@@ -92,12 +139,14 @@ if porch_length < 0.1:
         
     except Exception as error:
     cmds.warning("Failed to create porch: {}".format(error))
-      return none
+        return None
+
+    return porch
 
 def create_roof(roof_width=floor_width+wall_thickness+0.1,
                roof_height=0.5,
                roof_length=floor_length+wall_thickness+0.1
-               ):
+               )):
     """Creates a rectangular roof
 Args:
         roof_width (float):    Roof width. Default 5.
@@ -114,7 +163,7 @@ if DEBUG:
 
 if roof_width < body_width
     cmds.warning("invalid roof length {} -- using default 5".format(wall_length))
-    roof_width=body_width
+    roof_width=body_width+wall_thickness+0.1
 if roof_height < 0.1:
     cmds.warning("invalid wall height {} -- using default 0.5".format(wall_height))
     roof_height=0.5
@@ -134,7 +183,7 @@ try:
       cmds.warning("Failed to create roof: {}".format(error))
       return None
 
-    return wall
+    return roof
 
 def create_chimney(chimney_radius=0.2, chimney_height=1,
                    chimney_top_radius=chimney_radius * 1.5,
