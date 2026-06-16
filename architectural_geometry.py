9203c1386476cdf49e2ea1e72966d90fe24e3a80
AntonYemelianov <ey355@drexel.edu>
2026-06-15 20:51:01 -0400

    Update architectural_geometry.py

--- architectural_geometry.py
+++ architectural_geometry.py
@@ -17,7 +17,9 @@ import random
 DEBUG = True
 
 
-def create_body(body_width=5.0, body_height=0.3, body_length=5.0,
+def create_body(body_width=5.0,
+                body_height=0.3,
+                body_length=5.0,
                 position=(0, 0 ,0)):
     """Create a rectangular body of the building
 
@@ -62,14 +64,16 @@ def create_body(body_width=5.0, body_height=0.3, body_length=5.0,
 
     return body
 
-def create_door(door_width=0.5, door_height=1, door_thickness=0.1,
+def create_door(door_width=0.5,
+                door_height=1,
+                door_thickness=0.1,
                 position=(0, 0, 0)):
-    """Create a rectangular body of the building
+    """Create a rectangular door
 
     Args:
-        door_width (float):     Body width. Default 0.5.
-        door_height (float):    Body Height. Default 1.
-        door_thickness (float): Body length. Default 0.1.
+        door_width (float):     Door width. Default 0.5.
+        door_height (float):    Door Height. Default 1.
+        door_thickness (float): Door length. Default 0.1.
         position (tuple):  (x, y, z) center of the body.
 
     Returns:
@@ -99,7 +103,7 @@ def create_door(door_width=0.5, door_height=1, door_thickness=0.1,
         cmds.move(
             position[0],
             position[1] + door_height / 2.0 + porch_height,
-            position[2] + body_length / 2.0,
+            position[2] + body_length / 2.0 + door_thickness / 2.0,
             door
         )
     except Exception as error:
@@ -150,9 +154,9 @@ def create_window(window_width=0.5,
         random_displace=random.choice([displace1, displace2])
       
         cmds.move(
-            position[0] + 
+            position[0] + random_displace,
             position[1] + window_height / 2.0 + porch_height,
-            position[2] + window_length / 2.0,
+            position[2] + body_length / 2.0 + window_length / 2.0,
             window
         )
     except Exception as error:
@@ -163,156 +167,172 @@ def create_window(window_width=0.5,
         
       
 
-def create_porch(porch_width=1, porch_height=0.5, porch_width=1,
+def create_porch(porch_width=1, porch_height=0.5, porch_length=1,
                  stair_width=porch_width / 2.0,
                  stair_height=porch height / 4.0,
+                 stair_length=porch_length / 10.0
                  position=(0, 0 ,0)):
     """Create a rectangular porch with some stairs 
 
     Args:
-        porch_length (float):    Porch length. Default 5.
-        porch_height (float):    Porch height. Default 3.
-        porch_width (float): Floor length. Default 0.2.
-        position (tuple):  (x, y, z) center of the floor base.
+        porch_width (float):  Porch width. Default 1.
+        porch_height (float): Porch height. Default 0.5.
+        porch_length (float): Porch length. Default 1.
+        position (tuple):     (x, y, z) center of the porch base.
 
     Returns:
         Str: Name of the wall transform node or none if failure.
     """
-if DEBUG:
-    print("[DEBUG] create_porrch: pw={}, ph{}, pl={}, pos={}".format(
-      porch_width, porch_height, porch_thickness, position))
+    if DEBUG:
+        print("[DEBUG] create_porrch: pw={}, ph{}, pl={}, pos={}".format(
+        porch_width, porch_height, porch_thickness, position))
 
-if porch_width < 0.1:
-    cmds.warning("invalid porch width {} -- using default 5".format(porch_length))
-    wall_width = 1
-if porch_height < 0.05:
-    cmds.warning("invalid porch height {} -- using default 0.5".format(porch_height))
-    wall_height = 0.5
-if porch_length < 0.1:
-    cmds.warning("invalid porch thickness {} -- using default 5".format(porch_length))
-    porch_length = 10
+    if porch_width < 0.1:
+        cmds.warning("invalid porch width {} -- using default 5".format(porch_length))
+        wall_width = 1
+    if porch_height < 0.05:
+        cmds.warning("invalid porch height {} -- using default 0.5".format(porch_height))
+        wall_height = 0.5
+    if porch_length < 0.1:
+        cmds.warning("invalid porch thickness {} -- using default 5".format(porch_length))
+        porch_length = 10
 
-try:
-    porch = cmds.polyCube(width=porch_width, 
-                          height=porch_height,
-                          depth=porch_length,
-                          name="porch_#"
-    )[0]
+    try:
+        porch = cmds.polyCube(width=porch_width, 
+            height=porch_height,
+            depth=porch_length,
+            name="porch_#"
+        )[0]
         cmds.move(
-            (position[0], position[1] + porch_height/ 2.0, position[2], porch
-        )
-        stair1 = cmds.polyCube(width=stair_width, 
-                          height=stair_height,
-                          depth=porch_length,
-                          name="stair_#
-        )                      
-        
+            (position[0],
+             position[1] + porch_height / 2.0,
+             position[2] + body_length / 2.0
+             porch
+        )    
     except Exception as error:
     cmds.warning("Failed to create porch: {}".format(error))
         return None
 
     return porch
 
-def create_roof(roof_width=floor_width+wall_thickness+0.1,
-               roof_height=0.5,
-               roof_length=floor_length+wall_thickness+0.1
-               )):
+def create_roof(roof_width=body_width+0.1,
+                roof_height=0.5,
+                roof_length=body_length+0.1,
+                position=(0, 0, 0)):
     """Creates a rectangular roof
-Args:
-        roof_width (float):    Roof width. Default 5.
-        wall_height (float):    Roof height. Default 3.
-        wall_length (float): Roof length. Default 5.
-        position (tuple):  (x, y, z) center of the roof base.
+    Args:
+        roof_width (float):  Roof width. Default body width + 0.1.
+        roof_height (float): Roof height. Default 0.5.
+        roof_length (float): Roof length. Default body length + 0.1.
+        position (tuple):    (x, y, z) center of the roof base.
 
     Returns:
         Str: Name of the roof transform node or none if failure.
     """
-if DEBUG:
-    print("[DEBUG] create_roof: rw={}, rh{}, rl={}, pos={}".format(
-      roof_width, roof_height, roof_length, position))
+    if DEBUG:
+        print("[DEBUG] create_roof: rw={}, rh{}, rl={}, pos={}".format(
+            roof_width, roof_height, roof_length, position))
 
-if roof_width < body_width
-    cmds.warning("invalid roof length {} -- using default 5".format(wall_length))
-    roof_width=body_width+wall_thickness+0.1
-if roof_height < 0.1:
-    cmds.warning("invalid wall height {} -- using default 0.5".format(wall_height))
-    roof_height=0.5
-if roof_length < :
-    cmds.warning("invalid wall thickness {} -- using default body_width * 1.2".format(wall_thickness))
-    roof_length=body_width
-try:
-      roof = cmds.polyCube(width=roof_width, 
-            height=roof_height, 
-            depth=roof_length,
-            name="roof_#"
+    if roof_width < body_width
+        cmds.warning("invalid roof length {} -- using default body length + 0.1".format(roof_width))
+        roof_width=body_width + 0.1
+    if roof_height < 0.1:
+        cmds.warning("invalid wall height {} -- using default 0.5".format(roof_height))
+        roof_height = 0.5
+    if roof_length < body_width
+        cmds.warning("invalid wall thickness {} -- using default body legth + 0.1".format(roof_length))
+        roof_length = body_length + 0.1
+    try:
+        roof = cmds.polyCube(width=roof_width, 
+                            height=roof_height, 
+                            depth=roof_length,
+                            name="roof_#"
         )[0]
-  cmds.move(
-      (position[0], position[1] / 2.0, position[2], roof
-  )
+        cmds.move(
+            position[0],
+            position[1] / 2.0 + body_height,
+            position[2], 
+            roof
+        )
     except Exception as error:
-      cmds.warning("Failed to create roof: {}".format(error))
-      return None
+        cmds.warning("Failed to create roof: {}".format(error))
+        return None
 
     return roof
 
-def create_chimney(chimney_radius=0.2, chimney_height=1,
+def create_chimney(chimney_base_radius=0.2,
+                   chimney_base_height=1,
                    chimney_top_radius=chimney_radius * 1.5,
                    chimney_top_height=chimney_height * 0.2,
                    position=(0, 0, 0)):
     """Create a chimney consisting of two cylinders
 
     Args:
-        chimney_radius(float): Chimney radius. Default 0.2.
-        chimney_height(float): Chimney's height. Default 1.
-        chimney_top_radius: Radius of the chimney topper. 
-        Default to chimney_top_radius=chimney_radius * 1.5
-
+        chimney_base_radius(float): Chimney radius. Default 0.2.
+        chimney_base_height(float): Chimney's height. Default 1.
+        position (tuple):           (x, y, z) center of the chimney base.
     Returns:
         str: Name of the gatehouse group, or None on failure.
     """
-if DEBUG:
-    print("[DEBUG] create_chimney: cr={}, ch={} pos={}".format(
-      chimney_radius, chimney_height, position))
+    if DEBUG:
+        print("[DEBUG] create_chimney: cr={}, ch={} pos={}".format(
+            chimney_radius, chimney_height, position))
 
-if roof_width < floor_length + wall_thickness:
-    cmds.warning("invalid roof length {} -- using default 5".format(wall_length))
-    roof_width=10
-if roof_height < 0.1:
-    cmds.warning("invalid wall height {} -- using default 0.5".format(wall_height))
-    roof_height=0.5
-if roof_height < 0.1:
-    cmds.warning("invalid wall thickness {} -- using default 5".format(wall_thickness))
-    roof_length=10
-try:
-      roof = cmds.polyCylinder(radius=chimney_radius, 
-            height=roof_height, 
-            
-            name="chimney_#"
+    if chimney_radius < 0.01
+        cmds.warning("invalid chimney radius {} -- using default 0.2".format(chimney_radius))
+        chimney_radius = 0.2
+    if chimney_base_height < 0.05:
+        cmds.warning("invalid chimney height {} -- using default 0.5".format(chimney_height))
+        chimney_base_height=1
+        
+    parts = []
+
+    try:
+        #base
+        chimney_base = cmds.polyCylinder(
+            radius=chimney_base_radius,
+            height=chimney_base_height,
+            name="chimney_base_#"
         )[0]
-  cmds.move(
-      (position[0], position[1] / 2.0, position[2], roof
-  )
+        cmds.move(
+            position[0],
+            position[1] + chimney_base_height / 2.0,
+            position[2],
+            chimney_base
+        )
+
+        #topper
+        chimney_top = cmds.polyCylinder(
+            radius=chimney_top_radius,
+            height=chimney_top_height,
+            name="chimney_top_#"
+        )[0]
+        cmds.move(
+            position[0],
+            position[1] + chimney_top_height / 2.0 + chimney_base_height,
+            position[2]
+        )
     except Exception as error:
         cmds.warning("Failed to create chimney: {}".format(error))
         return None
 
-    return chimney
+return cmds.group(chimney_base, chimney_top, name="chimney_#")
                      
 
 if __name__ == "__main__":
     cmds.file(new=True, force=True)
 
-    create_body
-    create_porch
-    create_roof
-    create_chimney
-    create_window
-    create_door
+    create_body(body_width=10, body_height=5, body_length=10, position=(0, 0, 0))
+    create_porch(porch_width=1, porch_height=0.2, porch_length=2, position=(0, 0, 0))
+    create_roof(roof_width=11, roof_height=0.1, roof_length=11, position=(0, 0, 0))
+    create_chimney(chimney_radius=0.3, chimney_height=0.2, position=(0, 0, 0))
+    create_window(window_thickness=1, window_height=1, window_width=1, position=(0, 0, 0))
+    create_door(door_width=thickness, door_height=2, door_width=1, position=(0, 0, 0))
 
     # Test error handling
     print("\n--- Error handling tests ---")
     create_body(body_width=-1, body_height=-1, body_length=-1) # should warn, use defaults
-
+    create_door(door_thickness=0, door_height=0.0001, door_width=-4
     cmds.viewFit(allObjects=True)
     print("house_geometry self-test complete!")
 
